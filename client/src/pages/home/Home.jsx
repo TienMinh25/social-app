c286c9a778bae9c8475f2f9d9bcfd6c190104969
TienMinh25 <letienminh2512@gmail.com>
2023-05-25 03:04:16 +0700

    Done darkmode for app and homepage

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
-import "./home.scss";
+import Stories from '../../components/stories/Stories';
+import Posts from '../../components/posts/Posts';
+import Share from '../../components/share/Share';
+import './home.scss';
 
 const Home = () => {
     return (
-    <div className="home">
-        Home
-    </div>
+        <div className="home">
+            <Stories />
+            <Share />
+            <Posts />
+        </div>
     );
 };
 
