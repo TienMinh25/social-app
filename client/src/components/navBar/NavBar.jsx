07c91760b751d4385e389e2610fd27dc20490896
TienMinh25 <letienminh2512@gmail.com>
2023-05-25 03:28:22 +0700

    Done UI

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="left">
                 <Link to="/" style={{ textDecoration: 'none' }}>
-                    <span>lamasocial</span>
+                    <span>Social Network</span>
                 </Link>
                 <HomeOutlinedIcon />
                 {darkMode ? <WbSunnyOutlinedIcon onClick={toggle} /> : <DarkModeOutlinedIcon onClick={toggle} />}
