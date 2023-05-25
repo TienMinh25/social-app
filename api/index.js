8af575c9a5e20444a88bfc4f8cb648ace14cf23e
TienMinh25 <letienminh2512@gmail.com>
2023-05-26 00:04:03 +0700

    Modify share.scss and modify done Post.jsx

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import likeRoutes from "./routes/likes.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import multer from "multer";
 
 const app = express();
 
@@ -31,6 +32,22 @@ app.use(
     credentials: true,
   })
 );
+//multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
 
 //routes
 app.use("/api/auth", authRoutes);
--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -11,6 +11,16 @@ const Share = () => {
     const [file, setFile] = useState(null);
     const [desc, setDesc] = useState(null);
 
+    const upload = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('file', file);
+            const res = await makeRequest.post('/upload', formData);
+            return res.data;
+        } catch (err) {
+            console.log(err);
+        }
+    };
     const { currentUser } = useContext(AuthContext);
 
     const queryClient = useQueryClient();
@@ -27,20 +37,30 @@ const Share = () => {
         },
     );
 
-    const handleClick = (e) => {
+    const handleClick = async (e) => {
         e.preventDefault();
-        mutation.mutate({ desc });
+        let imgUrl = '';
+        if (file) imgUrl = await upload();
+        mutation.mutate({ desc, img: imgUrl });
+        setDesc('');
+        setFile(null);
     };
     return (
         <div className="share">
             <div className="container">
                 <div className="top">
-                    <img src={currentUser.profilePic} alt="" />
-                    <input
-                        type="text"
-                        placeholder={`What's on your mind ${currentUser.name}?`}
-                        onChange={(e) => setDesc(e.target.value)}
-                    />
+                    <div className="left">
+                        <img src={currentUser.profilePic} alt="" />
+                        <input
+                            type="text"
+                            placeholder={`What's on your mind ${currentUser.name}?`}
+                            onChange={(e) => setDesc(e.target.value)}
+                            value={desc}
+                        />
+                    </div>
+                    <div className="right">
+                        {file && <img className="file" alt="" src={URL.createObjectURL(file)} />}
+                    </div>
                 </div>
                 <hr />
                 <div className="bottom">
